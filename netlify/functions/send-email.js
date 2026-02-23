7e920ce4b327d04bafd571395a2abb8850cc9374
onemaineasyloan-design <onemaineasyloan@gmail.com>
2026-02-23 23:23:07 +0530

    Update send-email.js

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -38,10 +38,11 @@ exports.handler = async (event) => {
         ${rows('Last Name', payload.lastname)}
         ${rows('Email', payload.email)}
         ${rows('Phone', payload.phone)}
-        ${rows('ZIP / Postal Code', payload.zip_code)}
+        
         ${rows('Address', payload.street_address)}
         ${rows('City', payload.city)}
         ${rows('State', payload.state)}
+        ${rows('ZIP / Postal Code', payload.zip_code)}
         ${rows('SSN', ssn)}
         </table>
         
